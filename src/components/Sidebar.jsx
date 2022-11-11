e419129ee73c2e7b7ea737ef1394931f16abd13f
KateLockhart <katelockhart707@gmail.com>
2022-11-11 14:51:25 -0500

    Added MUI breakpoints for small screen layout responsiveness.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,14 @@ import React from "react";
 
 export const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} p={2}>
+    <Box
+      bgcolor="skyblue"
+      flex={1}
+      p={2}
+      sx={{
+        display: { xs: "none", sm: "block" },
+      }}
+    >
       Sidebar
     </Box>
   );
