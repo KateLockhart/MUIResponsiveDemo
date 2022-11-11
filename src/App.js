7d894c6a3d22b71a062619a366c527124311ad95
Kate Lockhart <63356074+KateLockhart@users.noreply.github.com>
2022-11-11 14:15:36 -0500

    MUI basics and base app clean up.
    First section of video complete covering MUI basics of custom components, theme creation, and React app base clean up.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Add, Settings } from "@mui/icons-material";
+import { Button, styled, Typography } from "@mui/material";
 
 function App() {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  }));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Typography variant="h1" component="p">
+        Test text
+      </Typography>
+      <BlueButton>My Button</BlueButton>
+      <BlueButton>Another Button</BlueButton>
     </div>
   );
 }
