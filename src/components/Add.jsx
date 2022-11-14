0dda9a8f01b2d0499c42f85fa1483f0be8ab6510
KateLockhart <katelockhart707@gmail.com>
2022-11-14 16:15:16 -0500

    Create post button complete, modal form in progress.

---
+++ src/components/Add.jsx
@@ -0,0 +1,66 @@
+import { Add as AddIcon } from "@mui/icons-material";
+import {
+  Tooltip,
+  Fab,
+  styled,
+  Modal,
+  Box,
+  Typography,
+  Avatar,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
+
+function Add() {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="grey" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox onClick={(e) => setOpen(true)}>
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              src="https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1944&q=80"
+            />
+            <Typography variant="span">Ava</Typography>
+          </UserBox>
+        </Box>
+      </StyledModal>
+    </>
+  );
+}
+
+export default Add;
