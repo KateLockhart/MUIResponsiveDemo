fdf58ef3ba19cb8477ff54ecadbacf5acf24e6d9
KateLockhart <katelockhart707@gmail.com>
2022-11-14 14:50:40 -0500

    Online friends section of Rightbar completed, latest posts in progress.

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,11 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 export const Rightbar = () => {
@@ -10,7 +17,73 @@ export const Rightbar = () => {
         display: { xs: "none", sm: "block" },
       }}
     >
-      <Box position="fixed">Rightbar</Box>
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://material-ui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://material-ui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/6.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/7.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/8.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/7.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/8.jpg"
+          />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100}>
+          Latest Photos
+        </Typography>
+        <ImageList>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/breakfast.jpg"
+              alt="A breakfast scene of a cup of coffee, a plated croissant, and a plate of avocado toast with a fried egg on top."
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/burgers.jpg"
+              alt="A burger with a fries."
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/camera.jpg"
+              alt="A old fashioned box camera."
+            />
+          </ImageListItem>
+        </ImageList>
+      </Box>
     </Box>
   );
 };
