3869e803cfb75b430b8c9e622dc5b45872e9a34c
KateLockhart <katelockhart707@gmail.com>
2022-11-14 15:34:07 -0500

    Latest photos and conversations section of rightbar complete.

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -2,8 +2,13 @@ import {
   Avatar,
   AvatarGroup,
   Box,
+  Divider,
   ImageList,
   ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
   Typography,
 } from "@mui/material";
 import React from "react";
@@ -17,7 +22,7 @@ export const Rightbar = () => {
         display: { xs: "none", sm: "block" },
       }}
     >
-      <Box position="fixed">
+      <Box position="fixed" width={300}>
         <Typography variant="h6" fontWeight={100}>
           Online Friends
         </Typography>
@@ -60,10 +65,10 @@ export const Rightbar = () => {
             src="https://material-ui.com/static/images/avatar/8.jpg"
           />
         </AvatarGroup>
-        <Typography variant="h6" fontWeight={100}>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
           Latest Photos
         </Typography>
-        <ImageList>
+        <ImageList cols={3} rowHeight={100} gap={5}>
           <ImageListItem>
             <img
               src="https://material-ui.com/static/images/image-list/breakfast.jpg"
@@ -83,6 +88,84 @@ export const Rightbar = () => {
             />
           </ImageListItem>
         </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Latest Conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://material-ui.com/static/images/avatar/1.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Cindy Baker"
+                src="https://material-ui.com/static/images/avatar/3.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
